91b0a2fc690e192db63a6c0806342ed279e9c11d
MathangiG <gmathangibtech@gmail.com>
2021-08-11 09:49:42 +0530

    A work

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -5,5 +5,12 @@ public class AxisBank {
 		System.out.println("Deposite");
 
 	}
+	
+	public void methodOne() {
+		System.out.println("Method 1");
+	}
+	public void methodTwo() {
+		System.out.println("Method 2");
+	}
 
 }
